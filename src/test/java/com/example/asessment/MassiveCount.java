7d8ca0aa2c0ee84a6f2857400ae27a8414717972
Nikolai2019 <kolya.ruslyakov@gmail.com>
2023-09-07 19:31:00 +0500

    MassiveCount created

--- src/test/java/com/example/asessment/MassiveCount.java
+++ src/test/java/com/example/asessment/MassiveCount.java
@@ -1,25 +1,21 @@
 package com.example.asessment;
 
-import java.lang.reflect.Array;
-import java.util.List;
+import java.util.stream.IntStream;
 
 public class MassiveCount {
-//    public static void main(String[] args) {
-//        int[] mas1 = new int[20];
-//        for (int i = 0; i < mas1.length; i++) {
-//            if (i % 3 == 0) {
-//                System.out.println("Fizz");
-//            } else if () {
-//
-//            }
-//            {
-//                System.out.println(mas1[i]);
-//            }
-//
-//
-//            mas1[i] = i + 1;
-//            System.out.println(mas1[i]);
-//        }
-//    }
+    public static void main(String[] args) {
+        int[] mas1 = IntStream.rangeClosed(1, 20).toArray();
+        for (int i = 0; i < mas1.length; i++) {
+            if (mas1[i] % 3 == 0 && mas1[i] % 5 == 0) {
+                System.out.println("Fizz-Buzz");
+            } else if (mas1[i] % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (mas1[i] % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(mas1[i]);
+            }
+        }
+    }
 
 }
